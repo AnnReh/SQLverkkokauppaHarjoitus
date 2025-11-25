f495c4ff06f332781696bf810dbe53f215cdbffd
Koirakoodi <sahkoposti@example.com>
2025-11-25 12:42:25 +0200

    Projektiin lisätty etusivu (index.html) sekä tietokanta.php. Jälkimmäinen tiedosto vastaa tietokantayhteyden alustamisesta. Tiedosto tarjoaa koko projektin käyttämän yhden yhteyspisteen MySQL:ään.

---
+++ tietokanta.php
@@ -0,0 +1,13 @@
+<?php
+//Tiedosto yhdistää PHP sovellukseen MySQL-tietokantaan (XAMPPia käyttäen)
+$serverinimi = "localhost";
+$kayttajanimi = "root";
+$salasana = ""; 
+$tietokanta = "ecommerce"; // korvaa omalla tietokantasi nimellä
+
+$yhdista = new mysqli($serverinimi, $kayttajanimi, $salasana, $tietokanta);
+
+if ($yhdista->connect_error) {
+    die("Yhteys epäonnistui: " . $yhdista->connect_error);
+}
+?>
